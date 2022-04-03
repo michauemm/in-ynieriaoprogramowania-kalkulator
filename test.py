d1bf93e6dd50ee6e241339837f9cfb42453f1051
michauemm <102590836+michauemm@users.noreply.github.com>
2022-04-03 12:47:59 +0200

    Revert "nowa funkcjonalnosc"

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 def hello(name)
 return "Hello" + str(name)
  
-def odejmowanie(a,b):
-	wynik = float(a) - float(b)
+def dodaj(a,b):
+	wynik = float(a) + float(b)
 	return wynik
 
 pierwsza = input()
 druga = input()
 
-print (odejmij(pierwsza,druga))
+print (dodaj(pierwsza,druga))
 
